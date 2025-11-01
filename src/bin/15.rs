00edd45cd07425093db07238b4c73c092df495d4
AbelMaireg <abelrighthere@gmail.com>
2025-11-01 22:09:55 +0300

    solution: day-15 part-2

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,4 +1,10 @@
+use std::collections::HashSet;
+
 advent_of_code::solution!(15);
+
+fn move_(pos: (i64, i64), motion: (i64, i64)) -> (i64, i64) {
+    (pos.0 + motion.0, pos.1 + motion.1)
+}
 
 #[allow(dead_code)]
 struct Warehouse {
@@ -11,9 +17,13 @@ struct Warehouse {
 
 impl std::fmt::Display for Warehouse {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for row in &self.grid {
-            for &cell in row {
-                write!(f, "{}", cell)?;
+        for (y, row) in self.grid.iter().enumerate() {
+            for (x, &cell) in row.iter().enumerate() {
+                if (x as i64, y as i64).eq(&self.robot) {
+                    write!(f, "@")?;
+                } else {
+                    write!(f, "{}", cell)?;
+                }
             }
             writeln!(f)?;
         }
@@ -45,19 +55,7 @@ impl Warehouse {
             })
             .collect();
 
-        let motions = sections
-            .next()
-            .unwrap()
-            .lines()
-            .flat_map(str::chars)
-            .filter_map(|c| match c {
-                '>' => Some((1, 0)),
-                '<' => Some((-1, 0)),
-                '^' => Some((0, -1)),
-                'v' => Some((0, 1)),
-                _ => None,
-            })
-            .collect();
+        let motions = Self::parse_motion(sections.next());
 
         Self {
             width: grid[0].len() as i64,
@@ -68,12 +66,70 @@ impl Warehouse {
         }
     }
 
+    fn new2(input: &str) -> Self {
+        let mut robot: (i64, i64) = (0, 0);
+        let mut sections = input.split("\n\n");
+        let grid: Vec<Vec<char>> = sections
+            .next()
+            .unwrap()
+            .lines()
+            .enumerate()
+            .map(|(y, line)| {
+                line.chars()
+                    .enumerate()
+                    .flat_map(|(x, c)| match c {
+                        '.' => ['.', '.'],
+                        '#' => ['#', '#'],
+                        'O' => ['[', ']'],
+                        ch => {
+                            if ch == '@' {
+                                robot = (x as i64 * 2, y as i64)
+                            }
+                            ['.', '.']
+                        }
+                    })
+                    .collect()
+            })
+            .collect();
+
+        let motions = Self::parse_motion(sections.next());
+
+        Self {
+            width: grid[0].len() as i64,
+            height: grid.len() as i64,
+            grid,
+            robot,
+            motions,
+        }
+    }
+
+    fn parse_motion(motions: Option<&str>) -> Vec<(i64, i64)> {
+        motions
+            .unwrap()
+            .lines()
+            .flat_map(str::chars)
+            .filter_map(|c| match c {
+                '>' => Some((1, 0)),
+                '<' => Some((-1, 0)),
+                '^' => Some((0, -1)),
+                'v' => Some((0, 1)),
+                _ => None,
+            })
+            .collect()
+    }
+
     fn get(&self, pos: (i64, i64)) -> char {
         self.grid[pos.1 as usize][pos.0 as usize]
     }
 
     fn get_mut(&mut self, pos: (i64, i64)) -> &mut char {
         &mut self.grid[pos.1 as usize][pos.0 as usize]
+    }
+
+    fn swap(&mut self, pos1: (i64, i64), pos2: (i64, i64)) {
+        let temp = self.get(pos1);
+        *self.get_mut(pos1) = self.get(pos2);
+        *self.get_mut(pos2) = temp;
     }
 
     fn attempt(&mut self, motion: (i64, i64)) {
@@ -94,39 +150,111 @@ impl Warehouse {
         }
 
         *self.get_mut(np) = 'O';
-        self.robot = (self.robot.0 + motion.0, self.robot.1 + motion.1);
+        self.robot = move_(self.robot, motion);
         *self.get_mut(self.robot) = '.';
     }
 
-    fn exec(&mut self) {
-        for motion in self.motions.clone() {
-            self.attempt(motion);
+    fn attempt2(&mut self, motion: (i64, i64)) {
+        let next_pos = move_(self.robot, motion);
+
+        match self.get(next_pos) {
+            '.' => {
+                self.robot = next_pos;
+            }
+            '#' => {}
+            '[' | ']' => {
+                if motion.1 == 0 {
+                    let mut shifts = vec![next_pos];
+
+                    while let '[' | ']' = self.get(move_(*shifts.last().unwrap(), motion)) {
+                        shifts.push(move_(*shifts.last().unwrap(), motion));
+                    }
+                    shifts.push(move_(*shifts.last().unwrap(), motion));
+
+                    match self.get(*shifts.last().unwrap()) {
+                        '#' => {}
+                        '.' => {
+                            for i in (1..shifts.len()).rev() {
+                                self.swap(shifts[i - 1], shifts[i]);
+                            }
+                            self.swap(self.robot, shifts[0]);
+                            self.robot = move_(self.robot, motion);
+                        }
+                        _ => {}
+                    }
+
+                    return;
+                }
+
+                let mut shifts = vec![HashSet::from([self.robot])];
+
+                'stack: loop {
+                    let mut next_layer = HashSet::new();
+
+                    for &pos in shifts.last().unwrap() {
+                        match self.get((pos.0 + motion.0, pos.1 + motion.1)) {
+                            '.' => {}
+                            '[' | ']' => {
+                                next_layer.insert(move_(pos, motion));
+                            }
+                            '#' => break 'stack,
+                            _ => unreachable!(),
+                        }
+                    }
+
+                    if next_layer.is_empty() {
+                        for layer in shifts.iter().rev() {
+                            for &pos in layer {
+                                self.swap(pos, move_(pos, motion));
+                            }
+                        }
+
+                        self.robot = next_pos;
+                        break;
+                    }
+
+                    next_layer.clone().iter().for_each(|&i| {
+                        let p = match self.get(i) {
+                            '[' => (i.0 + 1, i.1),
+                            ']' => (i.0 - 1, i.1),
+                            _ => unreachable!(),
+                        };
+                        next_layer.insert(p);
+                    });
+
+                    shifts.push(next_layer);
+                }
+            }
+            _ => unreachable!(),
         }
     }
 
-    fn count_boxes(&self) -> u64 {
+    fn exec(&mut self, by: char, f: fn(&mut Self, (i64, i64))) -> Option<u64> {
+        for motion in self.motions.clone() {
+            f(self, motion);
+        }
+
         let mut count = 0;
         for (y, row) in self.grid.iter().enumerate() {
             for (x, &cell) in row.iter().enumerate() {
-                if cell == 'O' {
+                if cell == by {
                     count += y * 100 + x
                 }
             }
         }
 
-        count as u64
+        Some(count as u64)
     }
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
     let mut warehouse = Warehouse::new(input);
-    warehouse.exec();
-    // println!("{}", warehouse);
-    Some(warehouse.count_boxes())
+    warehouse.exec('O', Warehouse::attempt)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut warehouse = Warehouse::new2(input);
+    warehouse.exec('[', Warehouse::attempt2)
 }
 
 #[cfg(test)]
@@ -141,9 +269,7 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file_part(
-            "examples", DAY, 2,
-        ));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(9021));
     }
 }
